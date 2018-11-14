4fe7153b7dfd5f2bd757a96949ae4379ebadf79b
munbenoit <mun.benoit@gmail.com>
2018-11-14 09:09:16 +0100

    Fix a problem when adding pizza

--- public/js/pizza.js
+++ public/js/pizza.js
@@ -6,20 +6,14 @@ $(document).ready(function() {
         var fromage = $("#fromage").val();
         var accompagnement = $("#accompagnement").val();
         var file = $("#file").val();
-        var piquante = $("#piquante").value;
+        console.log($("#file"))
+        var piquante = $("#piquante").is(':checked');
         console.log(name, sauce, viande, fromage, accompagnement, file, piquante);
         $.post('/new_pizza',{name, sauce, viande, fromage, accompagnement, file, piquante},function(data, status){
             if(status==='success'){
-                /*$('#notify-icon').empty()
-                $('#notify-icon').append('check_circle_outline')
-                $('#notify-text').empty()
-                $('#notify-text').append('Pizza '+name+" créée")
-                console.log($('#notify-text').text())
-                $('#notify').css({"display": 'block',"background-color":"#48B548"})*/
+                console.log("Pizza créée")
                 $("#perso").load(window.location.href + " #perso" )
                 $('#modal1').modal('close');
-                
-                //window.setTimeout(function(){$('#notify').css("display","none")},3000)
             }
         }).error(function(res){
             console.log("error")
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,82 +1,9 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-var session = require('express-session');
-var bodyParser = require('body-parser');
+const formidable = require('formidable')
 const Joi = require('joi')
 
-/*
-var user=[]
-fs.writeFileSync("./user.json",JSON.stringify(user))*/
-
-//const userFile = require('./user.json')
-/*
-var pizza=[{
-  "id":0,
-  "name":"Steak & Cheese",
-  "url": "/static/images/FR_PDSC_fr_menu_2047.jpg",
-  "composition": ["Sauce tomate", "mozzarella","boulettes de boeuf assaisonnées","tomates fraîches", "origan"],
-  "type":"Incontournable"
-},
-{
-  "id":1,
-  "name":"Pepper beef",
-  "url": "/static/images/FR_PPBF_fr_menu_2762.jpg",
-  "composition": ["Sauce tomate", "mozzarella", "pommes de terre sautées", "boulettes de boeuf assaisonnées", "oignons", "assaisonnement au poivre"],
-  "type":"Incontournable"
-},
-{
-  "id":2,
-  "name":"Pepperoni",
-  "url": "/static/images/FR_PCHP_fr_menu_1846.jpg",
-  "composition": [ "Sauce tomate", "mozzarella", "saucisson pepperoni"],
-  "type":"Bon plan"
-},
-{
-  "id":3,
-  "name":"Merguez",
-  "url": "/static/images/FR_PSME_fr_menu_1846.jpg",
-  "composition": [ "Sauce tomate", "mozzarella", "merguez"],
-  "type":"Bon plan"
-},
-{
-  "id":4,
-  "name":"Margherita",
-  "url": "/static/images/FR_PMAR_fr_menu_1846.jpg",
-  "composition": [ "Sauce tomate", "mozzarella"],
-  "type":"Bon plan"
-},
-{
-  "id":5,
-  "name":"Chick'n Bacon",
-  "url": "/static/images/FR_PPBF_fr_menu_2762.jpg",
-  "composition": [ "Sauce barbecue", "mozzarella", "bacon", "poulet rôti", "oignons", "tomates fraîches", "oignons croustillants"],
-  "type":"Nouveauté"
-},
-{
-  "id":6,
-  "name":"Spicy Dallas",
-  "url": "/static/images/FR_PSDB_fr_menu_1836.jpg",
-  "composition": [ "Sauce barbecue", "mozzarella", "saucisson pepperoni", "oignons", "boulettes de boeuf assaisonnées", "poulet rôti", "bacon", "sauce Dallas"],
-  "type":"Classique"
-},
-{
-  "id":7,
-  "name":"Cannibale",
-  "url": "/static/images/FR_PCAN_fr_menu_2047.jpg",
-  "composition": [ "Sauce barbecue", "mozzarella", "poulet rôti", "merguez", "boulettes de boeuf assaisonnées"],
-  "type":"Classique"
-},
-{
-  "id":8,
-  "name":"4 Fromages",
-  "url": "/static/images/FR_P4FR_fr_menu_2142.jpg",
-  "composition": [ "Sauce tomate (ou crème fraîche légère)", "mozzarella", "chèvre", "Emmental", "Fourme d’Ambert A.O.P"],
-  "type":"Classique"
-}]
-
-fs.writeFileSync('./pizza.json',JSON.stringify(pizza))
-*/
 
 app.use(express.json())
 const router = express.Router();
@@ -87,79 +14,121 @@ router.get("/", (req, res) => {
 });
 
 router.get("/pizza", (req, res) => {
-  let data = fs.readFileSync('./pizza.json')
+  let data = fs.readFileSync('./src/storage/pizza.json')
   let pizza = JSON.parse(data)
   console.log(pizza)
   res.render("pizza.ejs",{pizza: pizza});
 });
 
 router.post("/create", (req, res) => {
-  const schema = {
-    name: Joi.string().required(),
-    password: Joi.string().required(),
-    email: Joi.string().email().required()
-  }
-  const validation = Joi.validate(req.body,schema)
-  if(validation.error){
-    res.status(400).send(validation.error)
-    return
-  }else{
-    let data = fs.readFileSync('./user.json')
-    let obj = JSON.parse(data)
-    let exist = obj.find(function(e){
-      return e.name === req.body.name
-    })
-    if(!exist){
-      const newuser = {
-        id: obj.length+1,
-        name: req.body.name,
-        password: req.body.password,
-        mail: req.body.email
-      }
-      obj.push(newuser)
-      fs.writeFile('./user.json', JSON.stringify(obj),(err, data) => {
-        if(err){
-          console.log(err)
-        }
+  if(!req.session.user){
+    const schema = {
+      name: Joi.string().required(),
+      password: Joi.string().required(),
+      email: Joi.string().email().required()
+    }
+    const validation = Joi.validate(req.body,schema)
+    if(validation.error){
+      res.status(400).send(validation.error)
+      return
+    }else{
+      let data = fs.readFileSync('./src/storage/user.json')
+      let obj = JSON.parse(data)
+      let exist = obj.find(function(e){
+        return e.name === req.body.name
       })
-      res.send(newuser)
+      if(!exist){
+        const newuser = {
+          id: obj.length+1,
+          name: req.body.name,
+          password: req.body.password,
+          mail: req.body.email
+        }
+        obj.push(newuser)
+        fs.writeFile('./src/storage/user.json', JSON.stringify(obj),(err, data) => {
+          if(err){
+            console.log(err)
+          }
+        })
+        res.send(newuser)
+      }
+      else{
+        res.status(409).send("Utilisateur déjà existant")
+      }
     }
-    else{
-      res.status(409).send("Utilisateur déjà existant")
-    }
+  }else{
+    res.status(403).send("Vous êtes déjà connecté")
   }
 })
 
 router.post('/login',(req, res)=>{
-  const schema = {
-    name: Joi.string().required(),
-    password: Joi.string().required(),
-  }
-  const validation = Joi.validate(req.body, schema)
-  if(validation.error){
-    res.status(400).send(validation.error)
-    return
-  }else{
-    let data = fs.readFileSync('./user.json')
-    let obj = JSON.parse(data)
-    let index = obj.findIndex(function(e){
-      return e.name === req.body.name
-    })
-    if(index===-1){
-      res.status(401).send("Utilisateur inexistant")
+  if(!req.session.user){
+    const schema = {
+      name: Joi.string().required(),
+      password: Joi.string().required(),
+    }
+    const validation = Joi.validate(req.body, schema)
+    if(validation.error){
+      res.status(400).send(validation.error)
+      return
     }else{
-      if(req.body.password===obj[index].password){
-        req.session.user = obj[index]
-        res.send("Connected")
+      let data = fs.readFileSync('./src/storage/user.json')
+      let obj = JSON.parse(data)
+      let index = obj.findIndex(function(e){
+        return e.name === req.body.name
+      })
+      if(index===-1){
+        res.status(401).send("Utilisateur inexistant")
       }else{
-        res.status(401).send("Mot de passe invalide")
+        if(req.body.password===obj[index].password){
+          req.session.user = obj[index]
+          res.send("Connected")
+        }else{
+          res.status(401).send("Mot de passe invalide")
+        }
       }
     }
+  }else{
+    res.status(403).send("Vous êtes déjà connecté")
   }
 })
+/*
+router.post('/new_pizza', (req, res)=>{
+  let data = fs.readFileSync('./src/storage/pizza.json')
+  let obj = JSON.parse(data)
+  let exist = obj.find(function(e){
+    return e.name === req.body.name
+  })
+  if(!exist){
+    const newpizza = {
+      id: obj.length+1,
+      name: req.body.name,
+      url: req.body.file,
+      composition: [req.body.sauce, ...req.body.viande, ...req.body.fromage, ...req.body.accompagnement],
+      type: "Personnalisé" 
+    }
+
+
+    if(req.body.piquante){
+      newpizza.composition.push("Sauce piquante")
+    }
+    obj.push(newpizza)
+    console.log(obj)
+    fs.writeFile('./src/storage/pizza.json',JSON.stringify(obj),(err, data) => {
+      if(err){
+        console.log("erreur")
+      }
+    })
+    res.send(newpizza)
+  }
+  else{
+    res.status(409).send("Pizza déjà existante")
+  }
+
+})*/
 
 router.post('/new_pizza', (req, res)=>{
-  let data = fs.readFileSync('./pizza.json')
+  let data = fs.readFileSync('./src/storage/pizza.json')
   let obj = JSON.parse(data)
   let exist = obj.find(function(e){
     return e.name === req.body.name
@@ -174,7 +143,7 @@ router.post('/new_pizza', (req, res)=>{
     }
     obj.push(newpizza)
     console.log(obj)
-    fs.writeFile('./pizza.json',JSON.stringify(obj),(err, data) => {
+    fs.writeFile('./src/storage/pizza.json',JSON.stringify(obj),(err, data) => {
       if(err){
         console.log("erreur")
       }
